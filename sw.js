a2abe31d30e04db4673a1efe9e56c9ee8c8ffced
snow-qingyang <qingyang.bao@mail.utoronto.ca>
2025-05-20 10:38:44 -0400

    Site updated: 2025-05-20 10:38:44

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1747714998314";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1747751917863";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
